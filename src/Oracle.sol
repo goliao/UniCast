a47dba73e5bbb48856aba670c4c8145564c1d420
dementedgoat <arche.ether+dgoat@gmail.com>
2024-06-08 16:08:27 -0400

    adding liquidity data to oracle

--- src/Oracle.sol
+++ src/Oracle.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.8.25;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {PoolId} from "v4-core/types/PoolId.sol";
 
 contract Oracle is Ownable {
     uint128 public impliedVol;
@@ -12,6 +13,14 @@ contract Oracle is Ownable {
 
     error Unauthorized();
 
+    struct LiquidityData {
+        int24 tickLower;
+        int24 tickUpper;
+        int256 liquidityDelta;   
+    }
+
+    mapping (PoolId => LiquidityData) public liquidityData;
+
     modifier onlyKeeper() {
         if (msg.sender != keeper) revert Unauthorized();
         _;
@@ -19,6 +28,14 @@ contract Oracle is Ownable {
 
     constructor(address _keeper) Ownable(_keeper) {
         keeper = _keeper;
+    }
+
+    function setLiquidityData(PoolId _poolId, int24 _tickLower, int24 _tickUpper, int256 _liquidityDelta) external onlyKeeper {
+        liquidityData[_poolId] = LiquidityData({
+            tickLower: _tickLower,
+            tickUpper: _tickUpper,
+            liquidityDelta: _liquidityDelta
+        });
     }
 
     function setImpliedVol(uint128 _impliedVol) external onlyKeeper {
